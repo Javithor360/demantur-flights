e3665c4554561d875f0ed12541da3d10384a9dd0
Javithor360 <floresmejia004@gmail.com>
2024-03-24 00:15:35 -0600

    Aplicando los logos correctos

--- components/footer.php
+++ components/footer.php
@@ -4,6 +4,6 @@
     <div class="foot-container">
         <p class="copyright">Copyright &copy; Demantur 2024</p>
         <p class="copyright">Universidad Don Bosco</p>
-        <img src="../src/img/logo_full.png" alt="Logo">
+        <img src="../src/img/logo_full_white.png" alt="Logo">
     </div>
 </footer>
