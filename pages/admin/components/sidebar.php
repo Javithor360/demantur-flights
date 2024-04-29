1ab87e814a9633b314786816a7ac6e6add952a39
Javithor360 <floresmejia004@gmail.com>
2024-04-29 09:23:01 -0600

    Setup para dashboard del administrador [TO-DO] - Reacomodar la distribución de carpetas

---
+++ pages/admin/components/sidebar.php
@@ -0,0 +1,10 @@
+<aside>
+    <nav class="bg-[#31363F] text-white p-5 h-[100vh]">
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="users.php">Agregar destino</a></li>
+            <li><a href="posts.php">Agregar horario</a></li>
+            <li><a href="comments.php">Agregar vuelo</a></li>
+        </ul>
+    </nav>
+</aside>
---
+++ pages/admin/index.php
@@ -0,0 +1,19 @@
+<?php 
+
+$title = "Admin";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<?php require_once("../../components/headContent.php") ?>
+
+<body>
+    <main class="flex mr-5">
+        <?php require_once("components/sidebar.php") ?>
+        <section class="ml-4 p-4">
+            <h1>Admin</h1>
+        </section>
+    </main>
+</body>
+</html>
