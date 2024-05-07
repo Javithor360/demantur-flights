88c1b6d0b4957bc732d6fc1f84747617deabb6cf
Javithor360 <floresmejia004@gmail.com>
2024-05-07 10:10:11 -0600

    VISTA DE PAGOS CONECTADA Y FUNCIONAL

--- pages/infoCard.php
+++ pages/infoCard.php
@@ -7,13 +7,13 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"></link>
 </head>
 <body class="bg-gray-100">
-    <div class="min-h-screen flex items-center justify-center">
-        <div class="bg-white p-8 rounded-lg shadow-lg w-full max-w-md">
+    <div class="flex items-center justify-center min-h-screen">
+        <div class="w-full max-w-md p-8 bg-white rounded-lg shadow-lg">
             <div class="mb-4">
                 <img src="https://live.staticflickr.com/65535/53701536846_61daf52fa4_m.jpg" alt="Company logo with a red icon resembling a bird in flight, placed to the left of the company name 'Avianca' in red text." class="mb-6">
-                <h1 class="text-2xl font-bold mb-2">Ingresa tu tarjeta</h1>
-                <p class="text-gray-600 mb-6">Paga seguro con estos medios de pago</p>
-                <div class="flex space-x-2 mb-6">
+                <h1 class="mb-2 text-2xl font-bold">Ingresa tu tarjeta</h1>
+                <p class="mb-6 text-gray-600">Paga seguro con estos medios de pago</p>
+                <div class="flex mb-6 space-x-2">
                     <img src="https://upload.wikimedia.org/wikipedia/commons/5/5e/Visa_Inc._logo.svg" width="35" height="35" alt="VISA logo">
                     <img src="https://placehold.co/40x25?text=MC&bg=ffffff" width="35" height="35" alt="MasterCard logo">
                     <img src="https://placehold.co/40x25?text=AMEX&bg=ffffff" width="35" height="35" alt="American Express logo">
@@ -22,45 +22,45 @@
                 </div>
             </div>
             <div class="mb-4">
-                <label class="block text-gray-700 text-sm font-bold mb-2" for="cardholder-name">
+                <label class="block mb-2 text-sm font-bold text-gray-700" for="cardholder-name">
                     Nombre del titular
                 </label>
-                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="cardholder-name" type="text" placeholder="Nombre del titular">
+                <input class="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline" id="cardholder-name" type="text" placeholder="Nombre del titular">
             </div>
             <div class="mb-6">
-                <label class="block text-gray-700 text-sm font-bold mb-2" for="card-number">
+                <label class="block mb-2 text-sm font-bold text-gray-700" for="card-number">
                     Número de tarjeta
                 </label>
-                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="card-number" type="text" placeholder="Número de tarjeta">
+                <input class="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline" id="card-number" type="text" placeholder="Número de tarjeta">
             </div>
             <div class="flex justify-between mb-6">
-                <div class="w-1/3 mr-2">
-                    <label class="block text-gray-700 text-sm font-bold mb-2" for="expiration-month">
-                        Fecha de Expiración
+                <div class="w-1/2 mr-2">
+                    <label class="block mb-2 text-sm font-bold text-gray-700" for="expiration-month">
+                        Expiración
                     </label>
-                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="expiration-month" type="text" placeholder="Mes">
+                    <input class="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline" id="expiration-month" type="text" placeholder="Mes">
                 </div>
                 <div class="w-1/3 mx-2">
-                    <label class="block text-gray-700 text-sm font-bold mb-2" for="expiration-year">
+                    <label class="block mb-2 text-sm font-bold text-gray-700" for="expiration-year">
                         &nbsp;
                     </label>
-                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="expiration-year" type="text" placeholder="Año">
+                    <input class="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline" id="expiration-year" type="text" placeholder="Año">
                 </div>
                 <div class="w-1/3 ml-2">
-                    <label class="block text-gray-700 text-sm font-bold mb-2" for="cvv">
+                    <label class="block mb-2 text-sm font-bold text-gray-700" for="cvv">
                         &nbsp;
                     </label>
-                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="cvv" type="text" placeholder="CVV">
+                    <input class="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline" id="cvv" type="text" placeholder="CVV">
                 </div>
             </div>
             <div class="flex items-center justify-between">
                 <a href="./payments.php">
-                    <button class="bg-transparent hover:bg-gray-500 text-gray-700 font-semibold hover:text-white py-2 px-4 border border-gray-500 hover:border-transparent rounded" type="button">
+                    <button class="px-4 py-2 font-semibold text-gray-700 bg-transparent border border-gray-500 rounded hover:bg-gray-500 hover:text-white hover:border-transparent" type="button">
                         Volver
                     </button>
                 </a>
                 <a href="./purchaseSummary.php">
-                    <button class="bg-black hover:bg-gray-700 text-white font-bold py-2 px-4 rounded" type="button">
+                    <button class="px-4 py-2 font-bold text-white bg-black rounded hover:bg-gray-700" type="button">
                         Añadir tarjeta
                     </button>
                 </a>
--- pages/payments.php
+++ pages/payments.php
@@ -1,45 +1,9 @@
 <?php
 session_start();
-require_once $_SERVER['DOCUMENT_ROOT'] . '/DSS FINAL/demantur-flights/models/class.connection.php';
-
-$db = new Database();
-$conn = $db->connect();
-
-$id_horario = $_SESSION['id_horario'] ?? 1;
-$cantidad_personas = isset($_POST['cantidad_personas']) ? $_POST['cantidad_personas'] : $_SESSION['cantidad_personas'];
-$pasajeros = isset($_POST['pasajeros']) ? $_POST['pasajeros'] : (isset($_SESSION['pasajeros']) ? $_SESSION['pasajeros'] : null);
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    if (!empty($_POST['selectedSeats'])) {
-        $_SESSION['selectedSeats'] = $_POST['selectedSeats']; // Guardar los asientos en la sesión
-    }
-
-    // Otros procesamientos necesarios...
-}
-
-
-$detalleVuelo = null;
-$total = 0;
-
-if ($id_horario) {
-    $stmt = $conn->prepare("SELECT h.*, v.tarifa, d1.lugar as origen, d2.lugar as destino 
-                            FROM horario h 
-                            JOIN vuelo v ON h.id_horario = v.id_horario 
-                            JOIN destino d1 ON h.id_origen = d1.id_destino 
-                            JOIN destino d2 ON h.id_destino = d2.id_destino
-                            WHERE h.id_horario = :id_horario");
-    $stmt->bindParam(':id_horario', $id_horario, PDO::PARAM_INT);
-    $stmt->execute();
-    $detalleVuelo = $stmt->fetch(PDO::FETCH_ASSOC);
-
-    if ($detalleVuelo) {
-        $total = $cantidad_personas * $detalleVuelo['tarifa'];
-    }
-}
-
-$conn = null;
 ?>
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -48,23 +12,24 @@ $conn = null;
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
     <link rel="stylesheet" href="./assets/css/navfoot.css">
 </head>
+
 <body class="bg-gray-100">
     <?php include_once("./components/navbar_solo.php"); ?>
 
     <div class="container mx-auto px-4 py-24 min-h-[90vh]">
-        <div class="flex flex-col lg:flex-row justify-between bg-white shadow-lg rounded-lg overflow-hidden">
+        <div class="flex flex-col justify-between overflow-hidden bg-white rounded-lg shadow-lg lg:flex-row">
             <!-- Left side -->
-            <div class="w-full lg:w-2/3 p-8">
-                <h2 class="text-2xl font-semibold mb-2">Pagar y confirmar reserva</h2>
-                <p class="text-lg text-gray-600 mb-6">Elige tu método de pago favorito.</p>
+            <div class="w-full p-8 lg:w-2/3">
+                <h2 class="mb-2 text-2xl font-semibold">Pagar y confirmar reserva</h2>
+                <p class="mb-6 text-lg text-gray-600">Elige tu método de pago favorito.</p>
 
                 <form action="./infoCard.php" method="POST">
                     <div class="mb-6">
-                        <h3 class="text-lg font-semibold mb-4">Datos de facturación</h3>
-                        <input type="email" name="email" placeholder="Correo electrónico" class="w-full p-4 border rounded-lg mb-4" required>
-                        <input type="text" name="address" placeholder="Dirección de residencia" class="w-full p-4 border rounded-lg mb-4" required>
-                        <input type="text" name="city" placeholder="Ciudad" class="w-full p-4 border rounded-lg mb-4" required>
-                        <input type="text" name="country" placeholder="País" class="w-full p-4 border rounded-lg mb-4" required>
+                        <h3 class="mb-4 text-lg font-semibold">Datos de facturación</h3>
+                        <input type="email" name="email" placeholder="Correo electrónico" class="w-full p-4 mb-4 border rounded-lg" required>
+                        <input type="text" name="address" placeholder="Dirección de residencia" class="w-full p-4 mb-4 border rounded-lg" required>
+                        <input type="text" name="city" placeholder="Ciudad" class="w-full p-4 mb-4 border rounded-lg" required>
+                        <input type="text" name="country" placeholder="País" class="w-full p-4 mb-4 border rounded-lg" required>
                     </div>
 
                     <div class="flex items-center mb-6">
@@ -73,67 +38,63 @@ $conn = null;
                     </div>
 
                     <div class="flex items-center">
-                        <button class="bg-green-600 text-white px-6 py-3 rounded-lg font-semibold hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-600 focus:ring-opacity-50 cursor-pointer" type="submit">Confirmar y pagar</button>
-                        <p class="text-sm text-green-600 ml-4"><i class="fas fa-lock mr-1"></i>Usamos métodos de confianza para procesar tus pagos de forma segura.</p>
+                        <button class="px-6 py-3 font-semibold text-white bg-green-600 rounded-lg cursor-pointer hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-600 focus:ring-opacity-50" type="submit">Confirmar y pagar</button>
+                        <p class="ml-4 text-sm text-green-600"><i class="mr-1 fas fa-lock"></i>Usamos métodos de confianza para procesar tus pagos de forma segura.</p>
                     </div>
                 </form>
             </div>
 
             <!-- Right side -->
-            <div class="w-full lg:w-1/3 bg-gray-200 p-8 flex flex-col">
-                <h3 class="text-2xl font-semibold mb-4">Resumen de compra</h3>
-                <?php if ($detalleVuelo): ?>
-                <div class="mb-6 border-solid border-b-2 border-gray-300 pb-4">
-                    <h4 class="text-xl font-semibold mb-2">Vuelos</h4>
-                    <div class="mb-4">
-                        <div class="flex items-center">
-                            <i class="fas fa-plane-departure mr-2 text-black text-lg"></i>
-                            <p class="text-lg mb-1 font-semibold"><?= $detalleVuelo['origen'] . ' a ' . $detalleVuelo['destino']; ?></p>
-                        </div>
-                        <p class="text-base text-gray-600"><?= date('d-m-Y H:i', strtotime($detalleVuelo['hora_salida'])); ?> - <?= date('d-m-Y H:i', strtotime($detalleVuelo['hora_llegada'])); ?></p>
-                    </div>
-                    <div class="flex items-center">
-                        <i class="fa fa-users mr-2 text-black text-base"></i>
-                        <p class="text-base"><?= $cantidad_personas ?> pasajeros</p>
-                    </div>
-                </div>
-                <?php if ($pasajeros && is_array($pasajeros)) : ?>
-                    <div class="mb-6 border-solid border-b-2 border-gray-300 pb-4">
-                        <h4 class="text-xl font-semibold mb-2">Pasajeros</h4>
-                        <?php foreach ($pasajeros as $index => $pasajero) : ?>
+            <div class="flex flex-col w-full p-8 bg-gray-200 lg:w-1/3">
+                <h3 class="mb-4 text-2xl font-semibold">Resumen de compra</h3>
+                <?php if ($_SESSION['selected_flight']) : ?>
+                    <div class="pb-4 mb-6 border-b-2 border-gray-300 border-solid">
+                        <h4 class="mb-2 text-xl font-semibold">Vuelos</h4>
+                        <div class="mb-4">
                             <div class="flex items-center">
-                                <i class="fas fa-user mr-2 text-black text-lg"></i>
-                                <p class="text-lg mb-1 font-semibold"><?php echo htmlspecialchars($pasajero); ?></p>
+                                <i class="mr-2 text-lg text-black fas fa-plane-departure"></i>
+                                <p class="mb-1 text-lg font-semibold"><?= $_SESSION['selected_flight']['origen_lugar'] . ' a ' . $_SESSION['selected_flight']['destino_lugar']; ?></p>
                             </div>
-                        <?php endforeach; ?>
+                            <p class="text-base text-gray-600"><?= date('d/m/Y (H:i)', strtotime($_SESSION['selected_flight']['hora_salida'])); ?> - <?= date('d/m/Y (H:i)', strtotime($_SESSION['selected_flight']['hora_llegada'])); ?></p>
+                        </div>
+                        <div class="flex items-center">
+                            <i class="mr-2 text-base text-black fa fa-users"></i>
+                            <p class="text-base"><?= count($_SESSION['selected_passengers']) > 1 ? count($_SESSION['selected_passengers']) . " pasajeros" : "1 pasajero" ?></p>
+                        </div>
                     </div>
-                <?php endif; ?>
-                <div class="mb-6 border-solid border-b-2 border-gray-300">
-                    <h4 class="text-xl font-semibold mb-2">Detalle de compra</h4>
-                    <div class="mb-4 ml-6 text-gray-600">
-                    <div class="mb-4 ml-6 text-gray-600">
-    <?php 
-    // Decodificar JSON si es necesario y asegurarse de que es un arreglo antes de iterar
-    $selectedSeats = isset($_SESSION['selectedSeats']) ? $_SESSION['selectedSeats'] : [];
-    if (is_string($selectedSeats)) {
-        $selectedSeats = json_decode($selectedSeats, true);
-    }
+                    <?php if ($_SESSION['selected_passengers'] && is_array($_SESSION['selected_passengers'])) { ?>
+                        <div class="pb-4 mb-6 border-b-2 border-gray-300 border-solid">
+                            <h4 class="mb-2 text-xl font-semibold">Pasajeros</h4>
+                            <?php foreach ($_SESSION['selected_passengers'] as $index => $passengerData) {
+                                $passenger = $passengerData['names'] . " " . $passengerData['lastNames'];
+                            ?>
+                                <div class="flex items-center">
+                                    <i class="mr-2 text-lg text-black fas fa-user"></i>
+                                    <p class="mb-1 text-lg"><?php echo htmlspecialchars($passenger); ?></p>
+                                </div>
+                            <?php } ?>
+                        </div>
+                    <?php } ?>
+                    <div class="mb-6 border-b-2 border-gray-300 border-solid">
+                        <h4 class="mb-2 text-xl font-semibold">Detalle de compra</h4>
+                        <div class="mb-4 ml-6 text-gray-600">
+                            <div class="mb-4 ml-6 text-gray-600">
+                                <?php 
+                                    foreach ($_SESSION['selected_passengers'] as $index => $passengerData) {
+                                        $passenger = $passengerData['names'] . " " . $passengerData['lastNames'];
+                                        echo "<p>Asiento de pasajero #" . ($index + 1) . " (" . htmlspecialchars($passengerData['seat']) . ") - Precio: USD " . number_format($_SESSION['selected_flight']['tarifa'], 2) . "</p>";
+                                    }
+                                ?>
+                            </div>
 
-    foreach ($selectedSeats as $index => $seat) {
-        echo "<p>Asiento para pasajero " . ($index + 1) . ": " . htmlspecialchars($seat) .
-             " - Precio: USD " . number_format($detalleVuelo['tarifa'], 2) . "</p>";
-    }
-    ?>
-</div>
-
+                        </div>
                     </div>
-                </div>
 
-                <div class="mt-auto">
-                    <p class="text-2xl font-semibold">Total a pagar</p>
-                    <p class="text-4xl font-bold">USD <?= number_format($total, 2); ?></p>
-                </div>
-                <?php else: ?>
+                    <div class="mt-auto">
+                        <p class="text-2xl font-semibold">Total a pagar</p>
+                        <p class="text-4xl font-bold">USD <?= number_format(($_SESSION['selected_flight']['tarifa'] * count($_SESSION['selected_passengers'])), 2); ?></p>
+                    </div>
+                <?php else : ?>
                     <p>No se ha encontrado información del vuelo seleccionado.</p>
                 <?php endif; ?>
             </div>
@@ -142,4 +103,5 @@ $conn = null;
 
     <?php include_once("./components/footer.php"); ?>
 </body>
+
 </html>
